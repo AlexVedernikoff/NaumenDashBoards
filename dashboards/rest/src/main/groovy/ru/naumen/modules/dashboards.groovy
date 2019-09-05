92f8a43335fa79dbf1521d7da95d146337c4cbc9
ludmila.klueva <ludmila.klueva@nordclan.com>
2019-09-05 15:52:23 +0400

    SMRMEXT-10668 [BACKEND] Add request - get data sourse

--- dashboards/rest/src/main/groovy/ru/naumen/modules/dashboards.groovy
+++ dashboards/rest/src/main/groovy/ru/naumen/modules/dashboards.groovy
@@ -1,7 +1,3 @@
-package ru.naumen.modules
-
-import groovy.transform.Field
-
 /*! UTF-8 */
 //Автор: nordclan
 //Дата создания: 03.09.2019
@@ -12,21 +8,139 @@ import groovy.transform.Field
  */
 //Версия: 4.10.0.15
 //Категория: скриптовый модуль
+package ru.naumen.modules
+
+import groovy.transform.Field
+import groovy.transform.Immutable
+
+import static groovy.json.JsonOutput.toJson
 
 //region КОНСТАНТЫ
-@Field private static final String EXAMPLE_CONST = 'Hello'
+@Field private static final String MAIN_FQN = 'abstractBO'
+@Field private static final Collection<String> VALID_TYPE_ATTRIBUTE =
+        ['aggregate', 'object', 'dtInterval', 'date', 'dateTime', 'boLinks', 'catalogItemSet',
+         'backBOLinks', 'string', 'integer', 'catalogItem']
+//endregion
+
+//region КЛАССЫ
+/**
+ * Модель для источника данных
+ */
+@Immutable
+class DataSource
+{
+    /**
+     * Код типа метакласса
+     */
+    String fqnCode
+    /**
+     * Название источника данных
+     */
+    String title
+    /**
+     * Количество детей 1 уровня вложенности
+     */
+    int countChildren = 0
+}
+
+/**
+ * Модель для атрибута
+ */
+@Immutable
+class Attribute
+{
+    /**
+     * Код атрибута
+     */
+    String code
+    /**
+     * Название атрибута
+     */
+    String title
+    /**
+     * Тип атрибута
+     */
+    String type
+    /**
+     * Свойство атрибута (метаклассы ссылочных атрибутов, значения элементов справочника и т.д)
+     */
+    String property
+}
 //endregion
 
 //region REST-МЕТОДЫ
-String methodForInvokeFromRest(String name)
+/**
+ * Отдает список источников данных 1 уровня
+ * @param fqnCode код метакласса
+ * @return json список источников данных {заголовок, код, количество детей 1 уровня}
+ */
+String getDataSources(fqnCode = MAIN_FQN)
 {
-    return "$EXAMPLE_CONST, ${methodForInternalUsage(name)}"
+    def children = getMetaClassChildren(fqnCode)
+    Collection<DataSource> dataSources = mappingDataSource(children)
+    return toJson(dataSources)
+}
+
+/**
+ * Отдает список атрибутов для источника данных
+ * @param fqnCode код метакласса
+ * @return json список атрибутов {заголовок, код, тип атрибута}
+ */
+String getAttributesDataSources(fqnCode){
+    def attributes = api.metainfo.getMetaClass(fqnCode).attributes
+    Collection<Attribute> mappingAttributes = mappingAttribute(attributes)
+    return toJson(mappingAttributes)
 }
 //endregion
 
 //region ВСПОМОГАТЕЛЬНЫЕ МЕТОДЫ
-String methodForInternalUsage(String param)
+/**
+ * Временное решение для получения списка метаклассов и типов
+ * @param fqn код метакласса
+ * @return список детей 1 уровня
+ */
+private def getMetaClassChildren(String fqn)
 {
-    return param.toUpperCase()
+    Closure classValidator = { clazz ->
+        !clazz.@metaClass.isHidden() && clazz.@metaClass.status.name() != 'REMOVED'
+    }
+    return api.metainfo.getMetaClass(fqn)?.children?.collectMany {
+        if (classValidator.call(it))
+        {
+            return [it]
+        }
+        else if (it.toString() == 'userEntity')
+        {
+            return it.children.findAll(classValidator)
+        }
+        return []
+    }
+}
+
+/**
+ * Маппинг из списка объектов, идентифицирующий метакласс в список источников данных
+ * Collection<fqn> -> Collection<DataSource>
+ */
+private Collection<DataSource> mappingDataSource(def fqns)
+{
+    return fqns.collect { fqn ->
+        new DataSource(fqn.code, fqn.title, fqn.children.size())
+    }
+}
+
+/**
+ * Маппинг из коллекция кодов всех атрибутов метакласса в Collection<Attribute>
+ * Collection<fqnAttr> -> Collection<Attribute>
+ */
+private Collection<Attribute> mappingAttribute(def attributes)
+{
+    return api.metainfo.getMetaClass('test').attributes
+            .findAll{it.type.code in VALID_TYPE_ATTRIBUTE}
+            .collect { attribute ->
+                new Attribute(attribute.code,
+                        attribute.title,
+                        attribute.type.code,
+                        attribute.type.relatedMetaClass as String)
+            }
 }
 //endregion
