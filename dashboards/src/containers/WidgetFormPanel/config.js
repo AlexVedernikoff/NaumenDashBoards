7d6101468eefdf4cc14a02af5f55c15a34210fb6
KarbonRu <roman.khmurenko@nordclan.com>
2019-10-16 17:52:33 +0400

    SMRMEXT-10669 validation

--- dashboards/src/containers/WidgetFormPanel/config.js
+++ dashboards/src/containers/WidgetFormPanel/config.js
@@ -18,7 +18,7 @@ const config: FormikConfig = {
 		};
 	},
 
-	validationSchema: prop => lazy((values: ValidateType) => schema[values.type.value]),
+	validationSchema: () => lazy((values: ValidateType) => schema[values.type.value]),
 
 	handleSubmit: async (values: FormikValues, {props}: FormikProps) => {
 		const {createWidget, saveWidget, selectedWidget} = props;
