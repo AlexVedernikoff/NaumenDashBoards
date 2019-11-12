00b6a1092b8e4fa4c4ab7ffa292c76d8b3ee7ee9
Людмила Юрьевна Клюева <ludmila.klueva@nordclan.com>
2019-11-12 18:41:49 +0500

    SMRMEXT-10804 Отправка дашбордов на почту

---
+++ dashboards/rest/src/main/groovy/ru/naumen/modules/dashboardSendEmail.groovy
@@ -0,0 +1,39 @@
+/*! UTF-8 */
+//Автор: nordclan
+//Дата создания: 06.11.2019
+//Код:
+//Назначение:
+/**
+ * Бекенд для отправки email в встроенном приложении "Дашборды"
+ */
+//Версия: 4.10.0.15
+//Категория: скриптовый модуль
+
+package ru.naumen.modules
+
+import org.apache.poi.util.IOUtils
+import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest
+import javax.mail.util.ByteArrayDataSource
+import static groovy.json.JsonOutput.toJson
+
+
+//region REST-МЕТОДЫ
+String sendFileToMail(DefaultMultipartHttpServletRequest request, def user)
+{
+    if (!user?.email)
+    {
+        utils.throwReadableException('User email is null or empty!')
+    }
+    String fileName = request.getFileNames().find()
+
+    def file = request.getFiles(fileName).find()
+    def ds = new ByteArrayDataSource(file.inputStream, file.contentType)
+
+    def message = api.mail.sender.createMail()
+    message.addTo(user.title, user.email)
+    message.setSubject(fileName) //установка темы сообщения
+    message.addText("${fileName}. Файл с изображением дашборда находится во вложении.") //установка текста сообщения
+    message.attachFile(ds, fileName)
+    return toJson(api.mail.sender.sendMail(message))
+}
+//endregion
