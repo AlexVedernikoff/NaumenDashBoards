20b570a5b86df4c3d6e66d8c0b4747aa62b8fcbd
Kirill Zdorenko <kirill.zdorenko@nordclan.com>
2020-11-18 22:10:09 +0300

    SMRMEXT-12040 BUG Не применяется группировка, после редактирования ранее созданной группировки

--- dashboards/src/containers/GroupCreatingModal/selectors.js
+++ dashboards/src/containers/GroupCreatingModal/selectors.js
@@ -2,12 +2,16 @@
 import type {AppState} from 'store/types';
 import type {ConnectedFunctions, ConnectedProps} from './types';
 import {createCustomGroup, deleteCustomGroup, updateCustomGroup} from 'store/customGroups/actions';
+import {DIAGRAM_WIDGET_TYPES} from 'store/widgets/data/constants';
 import {getMapValues} from 'src/helpers';
 
 export const props = (state: AppState): ConnectedProps => {
 	const {customGroups, widgets: widgetsState} = state;
 	const {map, selectedWidget} = widgetsState.data;
-	const widgets = getMapValues(map).filter(widget => widget.id !== selectedWidget);
+	const widgets = getMapValues(map).filter(widget => {
+		const {id, type} = widget;
+		return id !== selectedWidget && type in DIAGRAM_WIDGET_TYPES;
+	});
 
 	return {
 		editableCustomGroups: customGroups.editable,
