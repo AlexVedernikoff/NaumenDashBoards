cebfcc2b9e65f9f8c9bef878a93c8702ae695943
aleksandr.korotaev <alexander.korotaev@nordclan.com>
2021-03-26 13:37:26 +0300

    SMRMEXT-11462 Доработка: SMRMEXT-11231 Иконки кастомных параметров в ссылочных типах атрибутов

--- dashboards/src/components/organisms/DiagramWidgetEditForm/components/AttributeGroupField/AttributeGroupField.jsx
+++ dashboards/src/components/organisms/DiagramWidgetEditForm/components/AttributeGroupField/AttributeGroupField.jsx
@@ -24,8 +24,8 @@ export class AttributeGroupField extends PureComponent<Props, State> {
 		const {attribute, value} = this.props;
 		const {CUSTOM, SYSTEM} = GROUP_WAYS;
 		const {DATE, NUMBER} = ATTRIBUTE_SETS;
-		const attributeType = attribute?.type;
-		const type = (attributeType !== 'object' ? attributeType : attribute?.ref?.type) ?? '';
+		const attributeType = attribute?.type ?? '';
+		const type = ((attributeType in ATTRIBUTE_SETS.OBJECT) ? attribute?.ref?.type : attributeType) ?? '';
 		const way = value && typeof value === 'object' && value.way === CUSTOM ? CUSTOM : SYSTEM;
 		const {calendar, number, text} = ICONS[way];
 
