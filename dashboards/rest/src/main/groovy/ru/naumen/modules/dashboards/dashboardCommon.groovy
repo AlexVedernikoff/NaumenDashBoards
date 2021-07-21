3028b1767ea5ef48b294fdbbd05db7301a38e395
Anna Tulisova <anna.tulisova@nordclan.com>
2021-07-21 13:03:00 +0400

    SMRMEXT-12474 (EXT-12495 Доработки по изменению метода компиляции)

--- dashboards/rest/src/main/groovy/ru/naumen/modules/dashboards/dashboardCommon.groovy
+++ dashboards/rest/src/main/groovy/ru/naumen/modules/dashboards/dashboardCommon.groovy
@@ -837,23 +837,6 @@ class DashboardUtils
         attrFormatToFind = attrFormatToFind.replace('_unitsLinks', '')
         return getModules().dynamicFields.getAttrToTotalValueMap()[attrFormatToFind]
     }
-
-    /**
-     * Метод, позволяющий получить минимальную дату у динамического атрибута типа дата/дата время
-     * @param attr - динамический атрибут типа дата/дата время
-     * @param source - источник запроса
-     * @return минимальная дата у динамического атрибута типа дата/дата время
-     */
-    static Date getMinDateDynamic(Attribute attr, Source source)
-    {
-        def sc =  getApi().selectClause
-        String templateUUID = attr.title //после обработки атрибута в модуле queryWrapper, значение uuid-а шаблона хранится в названии
-        def field = 'value'
-        def wrapper = QueryWrapper.build(source, templateUUID)
-        wrapper.totalValueCriteria.add(getApi().filters.attrValueEq('linkTemplate', templateUUID))
-               .addColumn(sc.min(sc.property(field)))
-        return wrapper.getResult(true, DiagramType.TABLE, true).flatten().head() as Date
-    }
 }
 
 //region КЛАССЫ
