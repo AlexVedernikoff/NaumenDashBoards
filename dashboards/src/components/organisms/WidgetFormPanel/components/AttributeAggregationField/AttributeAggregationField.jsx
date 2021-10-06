55c59734591d9dc822db7f5456cf08d923023a80
Коротаев Александр Владимирович <alexander.korotaev@nordclan.com>
2021-10-06 07:52:19 +0000

    SMRMEXT-12685 BUG Ошибка при перевыборе атрибута с другим набором агрегаций

--- dashboards/src/components/organisms/WidgetFormPanel/components/AttributeAggregationField/AttributeAggregationField.jsx
+++ dashboards/src/components/organisms/WidgetFormPanel/components/AttributeAggregationField/AttributeAggregationField.jsx
@@ -1,17 +1,44 @@
 // @flow
 import {getAggregationOptions} from './helpers';
 import MiniSelect from 'components/molecules/MiniSelect';
-import type {Props} from './types';
+import type {Props, State} from './types';
 import React, {PureComponent} from 'react';
 
-export class AttributeAggregationField extends PureComponent<Props> {
+export class AttributeAggregationField extends PureComponent<Props, State> {
 	static defaultProps = {
 		usesNotApplicableAggregation: false
 	};
 
+	state = {
+		options: getAggregationOptions(null, false)
+	};
+
+	checkValueInOptions = (value: string) => {
+		const {name, onSelect} = this.props;
+		const {options} = this.state;
+
+		if (options.findIndex(option => option.value === value) === -1) {
+			onSelect(name, options[0].value);
+		}
+	};
+
+	componentDidUpdate (prevProps: Props) {
+		const {attribute, usesNotApplicableAggregation, value} = this.props;
+
+		if (prevProps.attribute !== attribute || prevProps.usesNotApplicableAggregation !== usesNotApplicableAggregation) {
+			const options = getAggregationOptions(attribute, usesNotApplicableAggregation);
+
+			this.setState({options}, () => this.checkValueInOptions(value));
+		}
+
+		if (prevProps.value !== value) {
+			this.checkValueInOptions(value);
+		}
+	}
+
 	render () {
-		const {attribute, onSelect, renderValue, usesNotApplicableAggregation, value} = this.props;
-		const options = getAggregationOptions(attribute, usesNotApplicableAggregation);
+		const {onSelect, renderValue, value} = this.props;
+		const {options} = this.state;
 
 		return (
 			<MiniSelect
--- dashboards/src/components/organisms/WidgetFormPanel/components/AttributeAggregationField/types.js
+++ dashboards/src/components/organisms/WidgetFormPanel/components/AttributeAggregationField/types.js
@@ -1,12 +1,23 @@
 // @flow
 import type {Attribute} from 'store/sources/attributes/types';
+import {DEFAULT_AGGREGATION, INTEGER_AGGREGATION} from 'store/widgets/constants';
 import type {Node} from 'react';
 import type {RenderValueProps} from 'components/molecules//MiniSelect/types';
 
+export type AggrigationOption = {
+	label: string,
+	value: $Keys<typeof DEFAULT_AGGREGATION> | $Keys<typeof INTEGER_AGGREGATION>;
+};
+
 export type Props = {
 	attribute: Attribute | null,
+	name: string,
 	onSelect: (name: string, value: string) => void,
 	renderValue?: (props: RenderValueProps) => Node,
 	usesNotApplicableAggregation: boolean,
 	value: string
+};
+
+export type State = {
+	options: Array<AggrigationOption>
 };
