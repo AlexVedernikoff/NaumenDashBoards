abbccb2105897f16c7719a4ba79870ca611d176f
aleksandr.korotaev <alexander.korotaev@nordclan.com>
2021-10-11 13:42:07 +0300

    SMRMEXT-12714 При создании диаграмм без разбивки отключаем отображение легенды по умолчанию

--- dashboards/src/components/organisms/WidgetFormPanel/HOCs/withType/types.js
+++ dashboards/src/components/organisms/WidgetFormPanel/HOCs/withType/types.js
@@ -1,12 +1,16 @@
 // @flow
-import type {WidgetType} from 'store/widgets/data/types';
+import type {AnyWidget, WidgetType} from 'store/widgets/data/types';
+import NewWidget from 'store/widgets/data/NewWidget';
+import type {SetFieldValue, Values} from 'components/organisms/WidgetForm/types';
+
 export type Context = {
-	onChange: (type: string) => void,
+	onChange: (type: string, callback: () => mixed) => void,
 	value: WidgetType
 };
 
-export type InjectedProps = {
-	type: Context
+export type Props = {
+	setFieldValue: SetFieldValue,
+	type: Context,
+	values: Values,
+	widget: AnyWidget | NewWidget
 };
-
-export type Props = Object;
--- dashboards/src/components/organisms/WidgetFormPanel/components/WidgetSelectBox/WidgetSelectBox.jsx
+++ dashboards/src/components/organisms/WidgetFormPanel/components/WidgetSelectBox/WidgetSelectBox.jsx
@@ -1,14 +1,32 @@
 // @flow
+import {compose} from 'redux';
+import {DIAGRAM_FIELDS} from 'WidgetFormPanel/constants';
 import FormBox from 'components/molecules/FormBox';
 import FormField from 'components/molecules/FormField';
-import type {InjectedProps} from 'WidgetFormPanel/HOCs/withType/types';
+import NewWidget from 'store/widgets/data/NewWidget';
 import OuterSelect from 'components/molecules/OuterSelect';
+import type {Props} from 'WidgetFormPanel/HOCs/withType/types';
 import React, {PureComponent} from 'react';
 import {WIDGET_OPTIONS} from './constants';
+import {WIDGET_TYPES} from 'store/widgets/data/constants';
 import withType from 'WidgetFormPanel/HOCs/withType';
+import withValues from 'components/organisms/WidgetForm/HOCs/withValues';
+import withWidget from 'WidgetFormPanel/HOCs/withWidget';
 
-export class WidgetSelectBox extends PureComponent<InjectedProps> {
-	handleChange = (name: string, value: string) => this.props.type.onChange(value);
+export class WidgetSelectBox extends PureComponent<Props> {
+	handleChange = (name: string, value: string) => {
+		this.props.type.onChange(value, () => {
+			const {setFieldValue, values: {legend}, widget: {id}} = this.props;
+			const {BAR, COLUMN, LINE} = WIDGET_TYPES;
+			const targetTypes = [BAR, COLUMN, LINE];
+
+			if (id === NewWidget.id && value) {
+				const show = !targetTypes.includes(value);
+
+				setFieldValue(DIAGRAM_FIELDS.legend, {...legend, show});
+			}
+		});
+	};
 
 	render () {
 		const {value} = this.props.type;
@@ -23,4 +41,4 @@ export class WidgetSelectBox extends PureComponent<InjectedProps> {
 	}
 }
 
-export default withType(WidgetSelectBox);
+export default compose(withValues(DIAGRAM_FIELDS.legend), withWidget, withType)(WidgetSelectBox);
