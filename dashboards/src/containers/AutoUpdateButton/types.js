c52749b1e16703f0477301ae94495b0cb96bb638
Коротаев Александр Владимирович <alexander.korotaev@nordclan.com>
2021-10-13 08:20:51 +0000

    SMRMEXT-12717 Убираем проверку на роль в автообновлении

--- dashboards/src/containers/AutoUpdateButton/types.js
+++ dashboards/src/containers/AutoUpdateButton/types.js
@@ -1,11 +1,8 @@
 // @flow
 import type {AutoUpdateSettings} from 'store/dashboard/settings/types';
 import type {ThunkAction} from 'store/types';
-import type {UserRole} from 'store/context/types';
 
 export type ConnectedProps = {
-	personalDashboard: boolean,
-	role: UserRole,
 	settings: AutoUpdateSettings,
 };
 
