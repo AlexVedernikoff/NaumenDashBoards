9d6e703dc7d9c60e9073b3e41f922aac7a7fe3b2
Tkacen-ko <dmitry.tkachenko@nordclan.com>
2022-06-29 19:55:47 +0300

    EXT-13333 BUG Не строится виджет с параметром типа Вещественное число/Целое число/Строка(correction by comment 29.06.202214:08)

--- dashboards/rest/src/main/groovy/ru/naumen/modules/dashboards/dashboardQueryWrapper.groovy
+++ dashboards/rest/src/main/groovy/ru/naumen/modules/dashboards/dashboardQueryWrapper.groovy
@@ -1555,9 +1555,6 @@ class DashboardQueryWrapperUtils
                     }
                 }
                 break
-            case AttributeType.NUMBER_TYPES:
-                attribute.attrChains().last().ref = new Attribute(code: 'title', type: 'integer')
-                break
             default:
                 if (!(attributeType in AttributeType.ALL_ATTRIBUTE_TYPES))
                 {
