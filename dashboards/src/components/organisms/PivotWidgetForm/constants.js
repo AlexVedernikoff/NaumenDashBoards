49f994d656c5c40fb22205bba2bf3f56576b701c
Коротаев Александр Владимирович <alexander.korotaev@nordclan.com>
2022-08-17 12:06:11 +0500

    SMRMEXT-11357 Базовая схема для валидации сводной таблицы

--- dashboards/src/components/organisms/PivotWidgetForm/constants.js
+++ dashboards/src/components/organisms/PivotWidgetForm/constants.js
@@ -1,8 +1,38 @@
 // @flow
+import {array, baseSchema, mixed, object} from 'src/containers/DiagramWidgetForm/schema';
+import {boolean, lazy, string} from 'yup';
 import {DIAGRAM_FIELDS} from 'WidgetFormPanel/constants';
-import {object} from 'containers/DiagramWidgetForm/schema';
+
+const indicatorGrouping = object({
+	children: array().nullable().of(lazy(() => indicatorGrouping)),
+	hasSum: boolean(),
+	key: string().required(),
+	label: string().required(),
+	type: string().required()
+});
+
+const sourceLinks = object({
+	attribute: mixed().required(),
+	dataKey1: string().required(),
+	dataKey2: string().required()
+});
+
+const parametersOrder = object({
+	dataKey: string().required(),
+	parameter: object().required()
+});
 
 const schema = object({
+	...baseSchema,
+	data: array().of(object({
+		indicators: mixed().requiredByCompute(array().indicators(false)),
+		parameters: mixed().requiredByCompute(array().parameters(false)),
+		source: object().source()
+	})),
+	indicatorGrouping: array().nullable().of(indicatorGrouping),
+	links: array().nullable().of(sourceLinks),
+	parametersOrder: array().nullable().of(parametersOrder),
+	sources: mixed().minSourceNumbers()
 });
 
 export {
