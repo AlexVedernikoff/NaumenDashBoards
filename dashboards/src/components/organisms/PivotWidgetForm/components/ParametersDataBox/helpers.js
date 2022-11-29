8bafb97fb3c6cedf038692d168c09dcc9f725f64
Коротаев Александр Владимирович <alexander.korotaev@nordclan.com>
2022-11-29 15:25:08 +0300

    SMRMEXT-13531 Fix. Изменение логики в фиксе mr

--- dashboards/src/components/organisms/PivotWidgetForm/components/ParametersDataBox/helpers.js
+++ dashboards/src/components/organisms/PivotWidgetForm/components/ParametersDataBox/helpers.js
@@ -9,7 +9,7 @@ import type {DataSet} from 'store/widgetForms/pivotForm/types';
 export const hasDisableTotal = (data: Array<DataSet>): boolean => {
 	const indicators = data.flatMap(({indicators}) => indicators);
 	const aggregations = indicators.map(({aggregation}) => aggregation);
-	const disableByAggregations = aggregations.length === 0 || new Set(aggregations).size === 1;
+	const disableByAggregations = aggregations.length === 0 || new Set(aggregations).size !== 1;
 	const disableByIndicators = indicators.length === 0 || (indicators.length === 1 && !indicators[0].breakdown);
 
 	return disableByIndicators || disableByAggregations;
