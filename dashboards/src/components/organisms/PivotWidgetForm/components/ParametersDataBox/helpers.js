33d3e1607cc61d584bffc2526ff15dd512fc9395
Коротаев Александр Владимирович <alexander.korotaev@nordclan.com>
2022-11-30 14:42:01 +0300

    SMRMEXT-13531 Fix. Не для всех источников есть индикаторы

--- dashboards/src/components/organisms/PivotWidgetForm/components/ParametersDataBox/helpers.js
+++ dashboards/src/components/organisms/PivotWidgetForm/components/ParametersDataBox/helpers.js
@@ -6,8 +6,8 @@ import type {DataSet} from 'store/widgetForms/pivotForm/types';
  * @param {Array<DataSet>} data - источники
  * @returns {boolean} - признак на запрет формирования столбца "Итого"
  */
-export const hasDisableTotal = (data: Array<DataSet>): boolean => {
-	const indicators = data.flatMap(({indicators}) => indicators);
+export const hasDisableTotal = (data: Array<DataSet> = []): boolean => {
+	const indicators = data.flatMap(({indicators = []}) => indicators);
 	const aggregations = indicators.map(({aggregation}) => aggregation);
 	const disableByAggregations = aggregations.length === 0 || new Set(aggregations).size !== 1;
 	const disableByIndicators = indicators.length === 0 || (indicators.length === 1 && !indicators[0].breakdown);
