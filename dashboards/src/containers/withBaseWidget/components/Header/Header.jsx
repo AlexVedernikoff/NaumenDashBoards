aceb09c6f65753e463441846e671c5abcc7d5d0d
Коротаев Александр Владимирович <alexander.korotaev@nordclan.com>
2023-03-10 17:17:27 +0300

    SMRMEXT-13984 Явное отображение функции ТОП

--- dashboards/src/containers/withBaseWidget/components/Header/Header.jsx
+++ dashboards/src/containers/withBaseWidget/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 // @flow
+import type {Chart} from 'store/widgets/data/types';
 import cn from 'classnames';
-import {FONT_STYLES, TEXT_HANDLERS} from 'store/widgets/data/constants';
+import {FONT_STYLES, TEXT_HANDLERS, WIDGET_SETS, WIDGET_TYPES} from 'store/widgets/data/constants';
 import type {Props} from './types';
 import React, {createRef, PureComponent} from 'react';
 import type {Ref} from 'components/types';
 import settingsStyles from 'styles/settings.less';
+import styles from './styles.less';
 import WidgetTooltip from 'components/molecules/WidgetTooltip';
 
 export class Header extends PureComponent<Props> {
@@ -18,9 +20,54 @@ export class Header extends PureComponent<Props> {
 		this.props.onChangeHeight(this.headerRef.current?.clientHeight ?? 0);
 	}
 
+	renderName = () => {
+		const {widget} = this.props;
+		const {name, useName} = widget.header;
+
+		return useName ? widget.name : name;
+	};
+
+	renderTooltip = () => (<WidgetTooltip tooltip={this.props.widget.tooltip} />);
+
+	renderTop = () => {
+		const {widget} = this.props;
+
+		if (
+			widget
+			&& (
+				widget.type in WIDGET_SETS.AXIS
+				|| widget.type in WIDGET_SETS.CIRCLE
+				|| widget.type === WIDGET_TYPES.COMBO
+			)
+		) {
+			// $FlowFixMe
+			const chartWidget: Chart = widget;
+			const mainDataSet = chartWidget.data.find(ds => !ds.sourceForCompute);
+
+			if (mainDataSet && mainDataSet.top) {
+				const {count = 0, show = false} = mainDataSet.top ?? {};
+
+				if (show) {
+					return (
+						<div className={styles.top}>Топ {count}</div>
+					);
+				}
+			}
+		}
+
+		return null;
+	};;
+
 	render () {
-		const {className, settings, tooltip, widgetName} = this.props;
-		const {fontColor, fontFamily, fontSize, fontStyle, name, textAlign, textHandler, useName} = settings;
+		const {className, widget} = this.props;
+		const {
+			fontColor,
+			fontFamily,
+			fontSize,
+			fontStyle,
+			textAlign,
+			textHandler
+		} = widget.header;
 		const {BOLD, ITALIC, UNDERLINE} = FONT_STYLES;
 		const {CROP, WRAP} = TEXT_HANDLERS;
 		const nameCN = cn({
@@ -41,8 +88,9 @@ export class Header extends PureComponent<Props> {
 
 		return (
 			<div className={nameCN} ref={this.headerRef} style={style}>
-				{useName ? widgetName : name}
-				<WidgetTooltip tooltip={tooltip} />
+				{this.renderTop()}
+				{this.renderName()}
+				{this.renderTooltip()}
 			</div>
 		);
 	}
